39c33ff0bfee27a43f9705029adc9a37dc87a26d
Nhimm <129376164+cuongTTnguyen@users.noreply.github.com>
2026-08-12 01:43:39 +0700

    0143_12082026
    update vòng quay may mắn

--- admin-layout-helper.js
+++ admin-layout-helper.js
@@ -30,6 +30,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     <a href="admin-set-keo.html" class="admin-nav-item">
                         <i class="fas fa-tasks"></i> Quản lý set kèo
                     </a>
+                    <a href="admin-quay.html" class="admin-nav-item">
+                        <i class="fas fa-tasks"></i> Quản lý vòng quay
+                    </a>
                     <a href="index.html" class="admin-nav-item" style="margin-top: auto; border-top: 1px solid #34495e;">
                         <i class="fas fa-home"></i> Trở về Cửa hàng
                     </a>
